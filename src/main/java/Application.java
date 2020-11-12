f2475943e95bfb7956e9a5d3bc6afdc0943d8161
antonpanchishko <PanchishkoMayor@gmail.com>
2020-11-12 10:44:15 +0200

    4) locBranch: add code into class Application

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,5 +1,19 @@
+import java.util.Arrays;
+
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int [] values = new int[] {6, 7, 8, 7, 6, 6};
+        Arrays.sort(values);
+        int res = 0;
+        int count  = 0;
+        for  (int i = 0; i < values.length-1; i++){
+            if(values[i] == values[i+1]){
+                i++;
+                continue;
+            }
+            res = res + values[count]*10;
+            count++;
+        }
+        System.out.println(res);
     }
 }
